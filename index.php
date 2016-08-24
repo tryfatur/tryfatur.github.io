d9cdd6b08baf6d13c4dae3db561e192304bdc679
Try Fathur Rachman <tryfatur@gmail.com>
2016-08-24 11:25:27 +0700

    Update UI

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Portfolio</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+		<style type="text/css">
+			@import 'https://fonts.googleapis.com/css?family=Indie+Flower';
+
+			body {
+				background-color: #282828;
+				margin-top: 70px;
+				color: #fff;
+				font-family: 'Indie Flower', serif;
+				padding-bottom: 30px;
+			}
+			.avatar {
+				border-radius: 100px;
+			}
+
+			.col-md-6 > a:hover, a:visisted {
+				text-decoration: none;
+			}
+		</style>
+	</head>
+	<body>
+
+		<div class="text-center">
+			<img src="http://1.gravatar.com/avatar/943c9d07c3955b51d2060e3d88bf16ce?size=200" alt="" class="avatar">
+			<h1>Hai !</h1>
+			<h3>Perkenalkan, nama saya Try Fathur Rachman.</h3>
+			<h4>Saya seorang Backend Developer, Data Enthusiast </h4>
+			<h4>dan Rookie Open Government Researcher.</h4>
+			<hr style="width: 85px;" />
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3"></div>
+					<div class="col-md-6">
+						<i class="fa fa-fw fa-facebook fa-2x"></i>
+						<i class="fa fa-fw fa-linkedin fa-2x"></i>
+						<i class="fa fa-fw fa-github fa-2x"></i>
+						<i class="fa fa-fw fa-instagram fa-2x"></i>
+						<i class="fa fa-fw fa-wordpress fa-2x"></i>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+
+		<script src="http://code.jquery.com/jquery-3.1.0.slim.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</body>
+</html>
